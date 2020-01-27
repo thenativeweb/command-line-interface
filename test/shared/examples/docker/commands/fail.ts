3303b72ff7bd6c3d74228c0b16ad7cf1e91efedd
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2020-01-27 14:06:03 +0100

    Fix linter issues.

--- test/shared/examples/docker/commands/fail.ts
+++ test/shared/examples/docker/commands/fail.ts
@@ -1,17 +1,11 @@
 import { Command } from '../../../../../lib';
-import { ls } from './image/ls';
-import { rm } from './image/rm';
 
 const fail: Command<{}> = {
   name: 'fail',
   description: 'Fails.',
   optionDefinitions: [],
-  subcommands: {
-    ls,
-    rm
-  },
-  handle ({ options }): void {
-    throw Error('This command fails intentionally.');
+  handle (): void {
+    throw new Error('This command fails intentionally.');
   }
 };
 
